314038d493d75657ae201b772fbb0cbeee8bc47c
Misha-Otto <michael.zacharov@ottofeller.com>
2024-04-15 18:21:13 +0200

    update create project command, delete check team name, update schema, bump version, add rand crate

--- src/projects/create/mod.rs
+++ src/projects/create/mod.rs
@@ -1,13 +1,14 @@
 mod graphql;
+
+use chrono::{Duration, Utc};
 use crate::common::{
     authorization_headers::authorization_headers, colorful_theme::theme, config::Config,
-    execute_graphql_request::execute_graphql_request, fetch_user_id::fetch_user_id,
+    execute_graphql_request::execute_graphql_request,
     keyring::keyring, print_formatted_error::print_formatted_error,
 };
-use crate::projects::create::graphql::check_team_name::{check_team_name, CheckTeamName};
 use crate::projects::create::graphql::create_project::{create_project, CreateProject};
 use clap::Args;
-use dialoguer::{console::Style, theme::ColorfulTheme, Input, Sort};
+use dialoguer::{console::Style, theme::ColorfulTheme, Input, Sort, Confirm, Select};
 use graphql_client::GraphQLQuery;
 use reqwest::Client;
 use termimad::{
@@ -18,26 +19,22 @@ use termimad::{
 #[derive(Args, Debug)]
 pub struct ProjectsCreateArgs {
     #[clap(
-        short,
-        long,
-        help = "Project name, if not specified, the user will be prompted to enter the name"
+    short,
+    long,
+    help = "Project name, if not specified, the user will be prompted to enter the name"
     )]
     name: Option<String>,
     #[clap(
-        short,
-        long,
-        help = "Team name, if not specified, the project will be added to personal projects"
-    )]
-    team_name: Option<String>,
-    #[clap(
-        short,
-        long,
-        help = "Access token, if not specified, the token will be taken from the keychain"
+    short,
+    long,
+    help = "Access token, if not specified, the token will be taken from the keychain"
     )]
     access_token: Option<String>,
 }
 
 pub fn create(args: &ProjectsCreateArgs) {
+    let items_per_page = Config::new().items_per_page;
+
     let access_token = match &args.access_token {
         Some(token) => token.clone(),
         None => keyring::get("access_token"),
@@ -45,40 +42,6 @@ pub fn create(args: &ProjectsCreateArgs) {
 
     let client = Client::new();
     let headers = authorization_headers(&access_token);
-
-    // Check for the specified team (if not specified, set "Personal projects" by default)
-    let team_name = match &args.team_name {
-        Some(team) if !team.is_empty() => team.clone(),
-        _ => "Personal projects".to_string(),
-    };
-
-    let check_team_error_message = format!(
-        "Creation failed. The team '{}' not found.",
-        team_name.clone()
-    );
-
-    // Check the team name in the database
-    let team_response =
-        execute_graphql_request::<check_team_name::Variables, check_team_name::ResponseData>(
-            headers.clone(),
-            CheckTeamName::build_query,
-            &client,
-            &check_team_error_message,
-            check_team_name::Variables {
-                user_id: fetch_user_id(&access_token),
-                name: team_name.clone(),
-            },
-        )
-        .team;
-
-    let team_id = match team_response.len() {
-        1 => team_response[0].id,
-
-        _ => {
-            print_formatted_error(&check_team_error_message);
-            std::process::exit(1);
-        }
-    };
 
     let project_name = match &args.name {
         Some(name) => {
@@ -115,10 +78,96 @@ pub fn create(args: &ProjectsCreateArgs) {
         }
     };
 
+    let is_token_needed = match Confirm::with_theme(&theme())
+        .with_prompt("Do you want to generate a new token for this project?")
+        .interact()
+    {
+        Ok(token_needed) => token_needed,
+
+        Err(_) => {
+            print_formatted_error("Creation failed. Failed to read the token generation option.");
+            std::process::exit(1);
+        }
+    };
+
+    let token;
+
+    if is_token_needed {
+        let token_name = match Input::with_theme(&theme())
+            .with_prompt("Enter the token name:")
+            .validate_with(|input: &String| -> Result<(), &str> {
+                if input.trim().chars().count() < 2 {
+                    return Err("The token name must be at least 2 characters long.");
+                } else {
+                    Ok(())
+                }
+            })
+            .interact()
+        {
+            Ok(new_token_name) => new_token_name,
+
+            Err(_) => {
+                print_formatted_error("Creation failed. Failed to read the token name.");
+                std::process::exit(1);
+            }
+        };
+
+        let options = ["Endless", "7 days", "30 days", "60 days", "90 days"];
+
+        let expires_at_selections = match Select::with_theme(&theme())
+            .with_prompt(format!(
+                "Expires in: {}",
+                "Use <Up>/<Down> to navigate and <Enter>/<Space> to select".dark_grey()
+            ))
+            .default(0)
+            .items(
+                &options
+                    .iter()
+                    .map(|item| item.to_string())
+                    .collect::<Vec<String>>(),
+            )
+            .max_length(items_per_page)
+            .interact()
+        {
+            Ok(expires_at_selections) => expires_at_selections,
+
+            Err(_) => {
+                print_formatted_error("Sharing failed. Failed to read expiration time.");
+                std::process::exit(1);
+            }
+        };
+
+        let token_expires_in = match options[expires_at_selections] {
+            "Endless" => None,
+            "7 days" => Some((Utc::now() + Duration::days(7)).to_string()),
+            "30 days" => Some((Utc::now() + Duration::days(30)).to_string()),
+            "60 days" => Some((Utc::now() + Duration::days(60)).to_string()),
+            "90 days" => Some((Utc::now() + Duration::days(90)).to_string()),
+
+            _ => {
+                print_formatted_error("Creation failed. Failed to read expiration time.");
+                std::process::exit(1);
+            }
+        };
+
+        token = Some(create_project::TokenObject {
+            token_expires_in: token_expires_in,
+            token_name: Some(token_name),
+        });
+    } else {
+        token = None;
+    }
+
     let create_project_error_message = format!(
         "Creation failed. Failed to create a project with the name '{}'.",
         project_name.clone()
     );
+
+    let first_letters: String = project_name.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .take(2)
+        .flat_map(|c| c.to_uppercase())
+        .collect();
 
     // Create a project
     let project_response =
@@ -129,26 +178,18 @@ pub fn create(args: &ProjectsCreateArgs) {
             &create_project_error_message,
             create_project::Variables {
                 object: create_project::CreateProjectInput {
-                    name: project_name.clone(),
-                    active_team_id: team_id,
-                    icon: "star".to_string(), // Default icon
+                    project_icon: format!(">{}#{:02x}{:02x}{:02x}", first_letters, rand::random::<u8>(), rand::random::<u8>(), rand::random::<u8>()),
+                    project_name: project_name.clone(),
+                    token,
                 },
             },
         )
-        .create_project;
-
-    let project = match project_response {
-        Some(project) => project,
-
-        None => {
-            print_formatted_error(&create_project_error_message);
-            std::process::exit(1);
-        }
-    };
+            .create_project;
 
     // MD template for the project creation message
-    let text_template = minimad::TextTemplate::from(
-        r#"
+    let text_template = if is_token_needed {
+        minimad::TextTemplate::from(
+            r#"
         ##### ✔ Project successfully created
         ${description}
         **Project link**: ${project-link}
@@ -158,34 +199,42 @@ pub fn create(args: &ProjectsCreateArgs) {
         > *If you ever lose or forget this token, you can regenerate it.*
         > *However, be aware that any scripts or applications using this token will need to be updated accordingly.*
         "#,
-    );
-
-    let mut expander = text_template.expander();
-    let project_id = project.id.to_string();
-
-    let description = if &team_name == "Personal projects" {
-        format!(
-            "You created the project named '{}' as a personal.",
-            &project_name
         )
     } else {
-        format!(
-            "You created the project named '{}' within the '{}' team.",
-            &project_name, &team_name
+        minimad::TextTemplate::from(
+            r#"
+        ##### ✔ Project successfully created
+        ${description}
+        **Project link**: ${project-link}
+        **Project ID for CLI**: ${short-project-id}
+        "#,
         )
     };
+
+    let mut expander = text_template.expander();
+
+    let project_id = match project_response.project_id {
+        Some(id) => id,
+
+        None => {
+            print_formatted_error(&create_project_error_message);
+            std::process::exit(1);
+        }
+    };
+
+    let description = format!("You created the project named '{}'.", &project_name);
 
     let project_link = style(format!(
         "{}/projects/{}",
         Config::new().webapp_url,
         &project_id.to_string().replace("-", "")
     ))
-    .with(Color::Rgb {
-        r: 0,
-        g: 135,
-        b: 255,
-    })
-    .to_string();
+        .with(Color::Rgb {
+            r: 0,
+            g: 135,
+            b: 255,
+        })
+        .to_string();
 
     let styled_short_id = format!("{}", &project_id[..4].with(Color::Green));
     let styled_project_id = format!("{}", &project_id.with(Color::Green));
@@ -208,25 +257,27 @@ pub fn create(args: &ProjectsCreateArgs) {
         ..ColorfulTheme::default()
     };
 
-    match Sort::with_theme(&sort_theme)
-        .item(format!(
-            "{} '{}'. {}",
-            "Your token:",
-            style(format!("{}", &project.token)).with(Color::Green),
-            "Copy this token and press 'Enter' to remove it from the screen."
-        ))
-        .clear(true)
-        .report(false)
-        .interact()
-    {
-        Ok(confirmation) => confirmation,
+    if let Some(token_value) = project_response.token_value {
+        match Sort::with_theme(&sort_theme)
+            .item(format!(
+                "{} '{}'. {}",
+                "Your token:",
+                style(format!("{}", &token_value)).with(Color::Green),
+                "Copy this token and press 'Enter' to remove it from the screen."
+            ))
+            .clear(true)
+            .report(false)
+            .interact()
+        {
+            Ok(confirmation) => confirmation,
 
-        Err(_) => {
-            print_formatted_error(
-                &format!("Creation failed. Failed to display information about the created project with the name '{}'.", &project_name),
-            );
+            Err(_) => {
+                print_formatted_error(
+                    &format!("Creation failed. Failed to display information about the created project with the name '{}'.", &project_name),
+                );
 
-            std::process::exit(1);
-        }
-    };
+                std::process::exit(1);
+            }
+        };
+    }
 }

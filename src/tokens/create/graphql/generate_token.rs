caa9bc77a6626e3851c567591d33e30ea668a74d
Andrew Pahomov <andrey.pakhomov@ottofeller.com>
2024-04-25 23:54:57 +0800

    add `token create` command

---
+++ src/tokens/create/graphql/generate_token.rs
@@ -0,0 +1,48 @@
+#![allow(clippy::all, warnings)]
+pub struct GenerateToken;
+pub mod generate_token {
+    #![allow(dead_code)]
+    use std::result::Result;
+    pub const OPERATION_NAME: &str = "GenerateToken";
+    pub const QUERY : & str = "mutation GenerateToken($id: String!, $name: String!, $expiresAt: String) {\n  createProjectToken(\n    object: { projectId: $id, name: $name, expiresAt: $expiresAt }\n  ) {\n    tokenValue\n  }\n}\n" ;
+    use super::*;
+    use serde::{Deserialize, Serialize};
+    #[allow(dead_code)]
+    type Boolean = bool;
+    #[allow(dead_code)]
+    type Float = f64;
+    #[allow(dead_code)]
+    type Int = i64;
+    #[allow(dead_code)]
+    type ID = String;
+    #[derive(Serialize)]
+    pub struct Variables {
+        pub id: String,
+        pub name: String,
+        #[serde(rename = "expiresAt")]
+        pub expires_at: Option<String>,
+    }
+    impl Variables {}
+    #[derive(Deserialize)]
+    pub struct ResponseData {
+        #[serde(rename = "createProjectToken")]
+        pub create_project_token: GenerateTokenCreateProjectToken,
+    }
+    #[derive(Deserialize)]
+    #[derive(Debug)]
+    pub struct GenerateTokenCreateProjectToken {
+        #[serde(rename = "tokenValue")]
+        pub token_value: Option<String>,
+    }
+}
+impl graphql_client::GraphQLQuery for GenerateToken {
+    type Variables = generate_token::Variables;
+    type ResponseData = generate_token::ResponseData;
+    fn build_query(variables: Self::Variables) -> ::graphql_client::QueryBody<Self::Variables> {
+        graphql_client::QueryBody {
+            variables,
+            query: generate_token::QUERY,
+            operation_name: generate_token::OPERATION_NAME,
+        }
+    }
+}
--- src/tokens/mod.rs
+++ src/tokens/mod.rs
@@ -3,7 +3,13 @@
 ///  The `Tokens list` function displays information about the project tokens without hash value
 ///
 mod list;
+/// ## Create Tokens
+///
+///  The `Create list` function create new token for project
+///
+mod create;
 use crate::tokens::list::{list, TokenListArgs};
+use crate::tokens::create::{create, TokenCreateArgs};
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
@@ -17,10 +23,13 @@ pub struct TokenCli {
 pub enum TokenCommands {
     #[clap(about = "List all project token")]
     List(TokenListArgs),
+    #[clap(about = "Create project token")]
+    Create(TokenCreateArgs),
 }
 
 pub fn match_command(input: &TokenCli) {
     match &input.commands {
         TokenCommands::List(args) => list(&args),
+        TokenCommands::Create(args) => create(&args),
     }
 }

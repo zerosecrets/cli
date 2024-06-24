52739c57b335b21bfec25cd0688ba739853e76db
Andrew Pahomov <andrey.pakhomov@ottofeller.com>
2024-06-24 23:28:06 +0800

    update delete project token mutation

--- src/tokens/delete/graphql/delete_project_token.rs
+++ src/tokens/delete/graphql/delete_project_token.rs
@@ -4,7 +4,7 @@ pub mod delete_token {
     #![allow(dead_code)]
     use std::result::Result;
     pub const OPERATION_NAME: &str = "DeleteToken";
-    pub const QUERY : & str = "mutation DeleteToken($tokenId: uuid!) {\n    delete_projectToken_by_pk(id: $tokenId) {\n        id\n    }\n}\n" ;
+    pub const QUERY : & str = "mutation DeleteToken($tokenId: uuid!) {\n    delete_token_by_pk(id: $tokenId) {\n        id\n    }\n}\n" ;
     use super::*;
     use serde::{Deserialize, Serialize};
     #[allow(dead_code)]
@@ -15,26 +15,26 @@ pub mod delete_token {
     type Int = i64;
     #[allow(dead_code)]
     type ID = String;
+    type uuid = ::uuid::Uuid;
     #[derive(Serialize)]
     pub struct Variables {
         #[serde(rename = "tokenId")]
-        pub token_id: uuid::Uuid,
+        pub token_id: uuid,
     }
     impl Variables {}
     #[derive(Deserialize)]
     pub struct ResponseData {
-        #[serde(rename = "delete_projectToken_by_pk")]
-        pub delete_project_token_by_pk: Option<DeleteTokenDeleteProjectTokenByPk>,
+        pub delete_token_by_pk: Option<DeleteTokenDeleteTokenByPk>,
     }
     #[derive(Deserialize)]
-    pub struct DeleteTokenDeleteProjectTokenByPk {
-        pub id: uuid::Uuid,
+    pub struct DeleteTokenDeleteTokenByPk {
+        pub id: uuid,
     }
 }
 impl graphql_client::GraphQLQuery for DeleteToken {
     type Variables = delete_token::Variables;
     type ResponseData = delete_token::ResponseData;
-    fn build_query(variables: Self::Variables) -> graphql_client::QueryBody<Self::Variables> {
+    fn build_query(variables: Self::Variables) -> ::graphql_client::QueryBody<Self::Variables> {
         graphql_client::QueryBody {
             variables,
             query: delete_token::QUERY,

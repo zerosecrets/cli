533e00df65414685b5a96a893807fa2e27ef9417
Andrew Pahomov <andrey.pakhomov@ottofeller.com>
2024-10-04 19:20:54 +0800

    logout remove `user_id` out of keyring

--- src/auth/logout/mod.rs
+++ src/auth/logout/mod.rs
@@ -3,5 +3,6 @@ use termimad::crossterm::style::Stylize;
 
 pub fn logout() {
     keyring::delete("access_token");
+    keyring::delete("user_id");
     println!("{} {}", "✓".green(), "You have successfully logged out.");
 }

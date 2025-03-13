59dc08399f4a73f2aaa977133fa7c3360e54885e
Andrew Pahomov <andrey.pakhomov@ottofeller.com>
2025-03-13 18:48:22 +0800

    fix create project

--- src/projects/create/graphql/mod.rs
+++ src/projects/create/graphql/mod.rs
@@ -1,2 +1,2 @@
 pub mod create_project;
-pub mod team_slug;
+pub mod team_info_by_slug;
--- src/projects/create/graphql/team_info_by_slug.rs
+++ src/projects/create/graphql/team_info_by_slug.rs
@@ -1,11 +1,10 @@
 #![allow(clippy::all, warnings)]
-pub struct TeamSlug;
-pub mod team_slug {
+pub struct TeamInfoBySlug;
+pub mod team_info_by_slug {
     #![allow(dead_code)]
     use std::result::Result;
-    pub const OPERATION_NAME: &str = "TeamSlug";
-    pub const QUERY: &str =
-        "query TeamSlug($id: uuid!) {\n  team_by_pk(id: $id) {\n    id\n    slug\n  }\n}\n";
+    pub const OPERATION_NAME: &str = "TeamInfoBySlug";
+    pub const QUERY : & str = "query TeamInfoBySlug($slug: String!) {\n  team(where: { slug: { _eq: $slug } }) {\n    id\n  }\n}\n" ;
     use super::*;
     use serde::{Deserialize, Serialize};
     #[allow(dead_code)]
@@ -19,27 +18,26 @@ pub mod team_slug {
     type uuid = ::uuid::Uuid;
     #[derive(Serialize)]
     pub struct Variables {
-        pub id: uuid,
+        pub slug: String,
     }
     impl Variables {}
     #[derive(Deserialize)]
     pub struct ResponseData {
-        pub team_by_pk: Option<TeamSlugTeamByPk>,
+        pub team: Vec<TeamInfoBySlugTeam>,
     }
-    #[derive(Deserialize)]
-    pub struct TeamSlugTeamByPk {
+    #[derive(Deserialize, Debug)]
+    pub struct TeamInfoBySlugTeam {
         pub id: uuid,
-        pub slug: String,
     }
 }
-impl graphql_client::GraphQLQuery for TeamSlug {
-    type Variables = team_slug::Variables;
-    type ResponseData = team_slug::ResponseData;
+impl graphql_client::GraphQLQuery for TeamInfoBySlug {
+    type Variables = team_info_by_slug::Variables;
+    type ResponseData = team_info_by_slug::ResponseData;
     fn build_query(variables: Self::Variables) -> ::graphql_client::QueryBody<Self::Variables> {
         graphql_client::QueryBody {
             variables,
-            query: team_slug::QUERY,
-            operation_name: team_slug::OPERATION_NAME,
+            query: team_info_by_slug::QUERY,
+            operation_name: team_info_by_slug::OPERATION_NAME,
         }
     }
 }
